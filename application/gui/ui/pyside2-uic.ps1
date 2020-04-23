d8ad2cefdded54ec9a3101fe6e01bdaf368facf2
shotwn <anilhaksever@gmail.com>
2020-04-23 17:06:17 +0300

    remvoe pause

--- application/gui/ui/pyside2-uic.ps1
+++ application/gui/ui/pyside2-uic.ps1
@@ -1,8 +1,8 @@
-Get-ChildItem ".\" -Filter *.ui | 
+Get-ChildItem ".\" -Filter *.ui |
 Foreach-Object {
     pyside2-uic $_.FullName | Set-Content -Encoding UTF8 -Path ('..\generated\' + $_.BaseName + '.py')
     if($LASTEXITCODE -eq 1) {
         Pause
     }
 }
-Pause
+exit
